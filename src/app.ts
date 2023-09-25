f5e08dad7363990ea8a5ace2e5f13abc6f9a5fac
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-09-25 16:49:21 +0400

    fix: images from back fix

--- src/app.ts
+++ src/app.ts
@@ -17,19 +17,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/images/crew', express.static('assets/crew'));
-app.use('/images/destination', express.static('assets/destination'));
-app.use('/images/home', express.static('assets/home'));
-app.use('/images/shared', express.static('assets/shared'));
-app.use('/images/technology', express.static('assets/technology'));
+app.use('/images', express.static('public/assets'));
 
-app.get('/api/images', (req, res) => {
-    
-    const imageFiles = fs.readdirSync('assets');
-    
-   
-    res.json({ images: imageFiles });
-  });
+
+
 
 app.use("/api", destinationRouter)
 app.use("/api", crewRouter)
