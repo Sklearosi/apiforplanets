bec169efde3794effb715118d1b0da164cff3e57
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-09-26 16:21:25 +0400

    chore: images changed with assets

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/images', express.static('public/assets'));
+app.use('/assets', express.static('public/assets'));
 
 
 
